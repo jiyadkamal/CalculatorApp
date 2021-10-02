60cc26e13dc2706faed26e74f8ca439218e0c61f
jiyadkamal <91772422+jiyadkamal@users.noreply.github.com>
2021-10-02 10:45:48 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,22 @@
 import time
 
+
+def addition(x, y):
+    return x + y
+
+
+def subtraction(x, y):
+    return x - y
+
+
+def product(x, y):
+    return x * y
+
+
+def division(x, y):
+    return x / y
+
+
 while True:
 
     print("\nCALCULATOR")
@@ -16,30 +33,26 @@ while True:
         print("Enter two numbers:")
         x = int(input())
         y = int(input())
-        print(f'The sum of the two numbers is {x+y}')
+        print(f'The sum of the two numbers is {addition(x, y)}')
     elif user_choice == "2":
         print("Enter two numbers:")
         x = int(input())
         y = int(input())
-        print(f'The difference of the two numbers is {x-y}')
+        print(f'The difference of the two numbers is {subtraction(x, y)}')
     elif user_choice == "3":
         print("Enter two numbers:")
         x = int(input())
         y = int(input())
-        print(f'The product of the two numbers is {x*y}')
+        print(f'The product of the two numbers is {product(x, y)}')
     elif user_choice == "4":
         print("Enter two numbers:")
         x = int(input())
         y = int(input())
-        print(f'The quotient of two numbers is {x//y}')
+        print(f'The quotient of two numbers is {division(x, y)}')
     elif user_choice == "5":
         print("Program is exiting")
         time.sleep(2)
         break
     else:
         print("Invalid Option. Try Again")
-
-
-
-
-
+        
